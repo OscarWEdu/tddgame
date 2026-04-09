69919542254361ddd1497c7d4d253bb1927ae822
Ivan Kolokoltsev <ikolokoltsev@gmail.com>
2026-04-09 16:42:37 +0200

    Add bdd configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
+import { defineBddConfig } from "playwright-bdd";
 
 /**
  * Read environment variables from file.
@@ -11,8 +12,14 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+const bddTestDir = defineBddConfig({
+  features: "features/**/*.feature",
+  steps: "features/steps/**/*.ts",
+});
+
 export default defineConfig({
-  testDir: './tests',
+  testDir: "/tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,31 +29,49 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "e2e:chromium",
+      testDir: "./tests",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "bdd:chromium",
+      testDir: bddTestDir,
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "e2e:firefox",
+      testDir: "./tests",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "bdd:firefox",
+      testDir: bddTestDir,
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "e2e:webkit",
+      testDir: "./tests",
+      use: { ...devices["Desktop Safari"] },
+    },
+    {
+      name: "bdd:webkit",
+      testDir: bddTestDir,
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
