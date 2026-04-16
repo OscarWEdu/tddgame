9ee39fc411f79ff445520c2610d82fc9b0978a8a
jacoblasse <jacoblasse4@gmail.com>
2026-04-16 10:58:15 +0200

    Added POST in PlayersEndPoints

--- backend/src/Players/PlayersEndpoints.cs
+++ backend/src/Players/PlayersEndpoints.cs
@@ -13,11 +13,11 @@ public static class PlayersEndPoints
 {
     public static void MapPlayersEndpoints(this IEndpointRouteBuilder app)
     {
-        var playersEndpointsGroup = app.MapGroup("/api/game-session/{gameSessionId}/players").WithTags("Players");
+        var gameSessionPlayersGroup = app.MapGroup("/api/game-session/{gameSessionId}/players").WithTags("Players");
         var playerGroup = app.MapGroup("/api/players").WithTags("Players");
 
         //GET /api/game-session/{gameSession_id}/players
-        playersEndpointsGroup.MapGet(
+        gameSessionPlayersGroup.MapGet(
            "/",
            async Task<Ok<IEnumerable<PlayerDto>>> (int gameSessionId, IPlayersRepository repo, CancellationToken ct)
               =>
@@ -26,6 +26,17 @@ public static class PlayersEndPoints
                return TypedResults.Ok(player);
            }
         ).WithSummary("Get players in a game session").WithDescription("Return all players in a specific game session.");
+
+        //POST /api/game-session/{gameSession_id}/players
+        gameSessionPlayersGroup.MapPost(
+            "/",
+            async Task<Created<PlayerDto>> (int gameSessionId, CreatePlayerDto player, IPlayersRepository repo, CancellationToken ct)
+            =>
+            {
+                var createdPlayer = await repo.AddPlayerToGameAsync(gameSessionId, player, ct);
+                return TypedResults.Created($"/api/players/{createdPlayer.id}", createdPlayer);
+            }
+            ).WithSummary("Add a player to a game session").WithDescription("Create a new player in a specific game session.");
     }
 }
 
