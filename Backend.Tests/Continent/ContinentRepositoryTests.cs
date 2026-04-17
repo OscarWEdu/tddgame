d680b5fca0e45d1f79da4aa31fa4200f133f2da5
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-04-17 11:58:43 +0200

    Implement tests for the rest of the Continent repository methods

--- Backend.Tests/Continent/ContinentRepositoryTests.cs
+++ Backend.Tests/Continent/ContinentRepositoryTests.cs
@@ -14,7 +14,7 @@ public class ContinentRepositoryTests
 
     //Check that a continent is returned
     [Fact]
-    public async Task GetContinentAsync_ReturnsContinent()
+    public async Task GetContinentsAsync_ReturnsContinent()
     {
         var continents = new List<ContinentDto>
         {
@@ -24,8 +24,42 @@ public class ContinentRepositoryTests
 
         var results = await _mockRepo.Object.GetContinentsAsync(CancellationToken.None);
 
-
         Assert.NotNull(results);
         Assert.Single(results);
+    }
+
+    //Check that a continent is return when it exists, and that nothing returns when the request is invalid
+    [Theory]
+    [InlineData(0, false)]
+    [InlineData(1, true)]
+    public async Task GetContinentByIdAsync_ReturnsCorrectResponse(int testId, bool isValidRequest)
+    {
+        ContinentDto dto = new ContinentDto(1, "Test", 1);
+
+        _mockRepo.Setup(repo => repo.GetContinentByIdAsync(1, It.IsAny<CancellationToken>())).ReturnsAsync(dto);
+
+        var results = await _mockRepo.Object.GetContinentByIdAsync(testId, CancellationToken.None);
+
+        if (isValidRequest)
+        {
+            Assert.NotNull(results);
+            Assert.Equal("Test", results.name);
+        }
+        else { Assert.Null(results); }
+    }
+
+    //Checks that CreateContinent passes, and returns the created continent
+    [Fact]
+    public async Task CreateContinentAsync_ReturnsNewContinent()
+    {
+        ContinentDto dto = new ContinentDto(1, "test", 1);
+
+        _mockRepo.Setup(repo => repo.CreateContinentAsync("test", 1, It.IsAny<CancellationToken>())).ReturnsAsync(dto);
+
+        var results = await _mockRepo.Object.CreateContinentAsync("test", 1, CancellationToken.None);
+
+        Assert.NotNull(results);
+        Assert.Equal("test", results.name);
+        Assert.Equal(1, results.bonusConst);
     }
 }
