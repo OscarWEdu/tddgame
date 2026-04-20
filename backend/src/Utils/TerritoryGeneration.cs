8baf968c331f10436829cf6ae7d1658097756c16
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-04-20 11:31:36 +0200

    Run startuputils from app, and add a quick call to test it

--- backend/src/Utils/TerritoryGeneration.cs
+++ backend/src/Utils/TerritoryGeneration.cs
@@ -1,6 +1,11 @@
 namespace TddGame;
 
+using Microsoft.AspNetCore.Http.HttpResults;
+
 public static class TerritoryGeneration
 {
-    
+    public static async Task AddTerritories(int width, int height, ITerritoryRepository repo, CancellationToken ct)
+    {
+        await repo.CreateTerritoryAsync("Michigan", -1, -1, -1, -1, 1, ct);
+    }
 }
