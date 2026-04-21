24a1fff789a6ca184739322a74613f2b360957bf
Damasus Okeke <gwill2be@gmail.com>
2026-04-21 08:36:20 +0200

    Added battle implementation code, including xunit test code

--- backend/src/App.cs
+++ backend/src/App.cs
@@ -16,6 +16,7 @@ builder.Services.AddScoped<IPlayersRepository, PlayersRepository>();
 builder.Services.AddScoped<ITurnsRepository, TurnsRepository>();
 builder.Services.AddScoped<IContinentRepository, ContinentRepository>();
 builder.Services.AddScoped<ITerritoryRepository, TerritoryRepository>();
+builder.Services.AddScoped<IBattlesRepository, BattlesRepository>();
 
 builder.Services.AddCors(options =>
 {
@@ -44,6 +45,7 @@ app.MapPlayersEndpoints();
 app.MapTurnsEndpoints();
 app.MapContinentEndpoints();
 app.MapTerritoryEndpoints();
+app.MapBattlesEndpoints();
 
 try
 {
