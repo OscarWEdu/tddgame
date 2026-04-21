0aabea8c054c8445cc84e32d7f8d8a43d34bfd33
Meti00a <muhamedhajra00@gmail.com>
2026-04-21 11:57:46 +0200

    Implemented an ImissionRepository, so that it can fetch the first, current and the nextmission.

---
+++ backend/src/Missions/IMissionRepository.cs
@@ -0,0 +1,15 @@
+// mission repository implementation
+// Implements the repository interface using MySQL
+// this class contains mission related database operations such as fetching the current mission, creating a new mission and fetching the next mission
+// gives a mission dto as a result of the database operations
+namespace TddGame;
+//defines the contract for all mission based database operations
+public interface IMissionsRepository
+{
+  Task<MissionDto?> GetCurrentMissionByGameSessionIDAsync(string MissionId, CancellationToken ct);
+  Task<MissionDto?> CreateMissionAsync(CreateMissionDto turn, CancellationToken ct);
+  Task<int?> GetFirstMissionIdByGameSessionIdAsync(string MissionId, CancellationToken ct);
+  Task<int?> GetCurrentMissionAsync(string MissionID, int currentMission, CancellationToken ct);
+  Task<int?> GetNextMissionAsync(string MissionID, int NextMission, CancellationToken ct);
+
+}
