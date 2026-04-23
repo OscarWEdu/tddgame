398a468a4b5204a72e0a4b466f10790400dccae8
jacoblasse <jacoblasse4@gmail.com>
2026-04-23 11:28:47 +0200

    Added prettifyId function

--- src/components/RiskMap.tsx
+++ src/components/RiskMap.tsx
@@ -8,3 +8,7 @@ const territoryClass = "territory";
 function nameToSvgId(name: string) {
     return name.toLowerCase().replace(/\s+/g, "_");
 }
+
+function prettifyId(id: string) {
+    return id.split("_").map((part) => part.charAt(0).toUpperCase() + part.slice(1)).join(" ");
+}
