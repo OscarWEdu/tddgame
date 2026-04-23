172b2ea5c8ad720505550ef564ac578ca1b799c8
Meti00a <muhamedhajra00@gmail.com>
2026-04-23 14:33:31 +0200

    implemented and fixed MissionEndpoints.

---
+++ backend/src/Missions/MissionEndpoints.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace TddGame;
+
+public static class MissionEndpoints
+{
+  public static IEndpointRouteBuilder MapMissionEndpoints(this IEndpointRouteBuilder app)
+  {
+    var missionGroup = app.MapGroup("/api/missions").WithTags("Missions");
+
+    missionGroup.MapGet(
+        "/",
+        async Task<Results<Ok<IEnumerable<MissionDto>>, NotFound<string>>> (
+            IMissionsRepository repo,
+            CancellationToken ct) =>
+        {
+          var missions = await repo.GetMissionsAsync(ct);
+
+          if (missions is null)
+          {
+            return TypedResults.NotFound("No missions found.");
+          }
+
+          return TypedResults.Ok(missions);
+        })
+        .WithSummary("Get all missions")
+        .WithDescription("Fetch all missions.");
+
+    missionGroup.MapPost(
+        "/",
+        async Task<Results<Ok<MissionDto>, BadRequest<string>>> (
+            CreateMissionDto mission,
+            IMissionsRepository repo,
+            CancellationToken ct) =>
+        {
+          var createdMission = await repo.CreateMissionAsync(mission, ct);
+
+          if (createdMission is null)
+          {
+            return TypedResults.BadRequest("Could not create mission.");
+          }
+
+          return TypedResults.Ok(createdMission);
+        })
+        .WithSummary("Create mission")
+        .WithDescription("Create a new mission.");
+
+    missionGroup.MapGet(
+        "/{missionId}/{currentMission}",
+        async Task<Results<Ok<int>, NotFound<string>>> (
+            string missionId,
+            int currentMission,
+            IMissionsRepository repo,
+            CancellationToken ct) =>
+        {
+          var nextMission = await repo.GetMissionByPlayerIdAsync(missionId, currentMission, ct);
+
+          if (nextMission is null)
+          {
+            return TypedResults.NotFound("Mission not found.");
+          }
+
+          return TypedResults.Ok(nextMission.Value);
+        })
+        .WithSummary("Get mission by player id")
+        .WithDescription("Fetch mission using mission id and current mission.");
+
+    return app;
+  }
+}
